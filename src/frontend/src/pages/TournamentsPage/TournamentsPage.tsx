660d56233571bcfe00e4f67fe750892c76f7e91c
perttu123 <131781112+perttu123@users.noreply.github.com>
2025-05-23 18:22:29 +0300

    Deleted unnecessary index.ts files from pages and components.

--- src/frontend/src/pages/TournamentsPage/TournamentsPage.tsx
+++ src/frontend/src/pages/TournamentsPage/TournamentsPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import PageTemplate from '../../components/PageTemplate';
+import PageTemplate from '../../components/PageTemplate/PageTemplate';
 import './TournamentsPage.css';
 
 function TournamentsPage() {
