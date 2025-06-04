ca709cf085b84bdfc35965dba092c64639586743
JanToi <132272769+JanToi@users.noreply.github.com>
2025-06-04 23:49:26 +0300

    Removed unused import declarations to fix linter errors

--- src/frontend/src/router/routes.tsx
+++ src/frontend/src/router/routes.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import type { RouteObject } from 'react-router-dom';
 import HomePage from '../pages/HomePage/HomePage';
 import NewsPage from '../pages/NewsPage/NewsPage';
