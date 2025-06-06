b6998f4cc280ba75be906281a4d9c6a8e923cce2
Vilz01 <viltsu.soi@gmail.com>
2025-06-06 09:35:24 +0300

    removed unused imports from ClubPage.tsx

--- src/frontend/src/pages/ClubPage/ClubPage.tsx
+++ src/frontend/src/pages/ClubPage/ClubPage.tsx
@@ -3,8 +3,6 @@ import { useParams } from 'react-router-dom';
 import PageTemplate from '../../components/PageTemplate/PageTemplate';
 import type { Club } from '../../api/clubService';
 import { getClubs } from '../../api/clubService';
-//import clubData from '../../sampledata/club_data.json';
-//import { slugify } from '../../utils/helpers';
 import './ClubPage.scss';
 
 function ClubPage() {
