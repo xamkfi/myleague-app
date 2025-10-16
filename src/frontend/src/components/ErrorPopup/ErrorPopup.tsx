1d1211d52011ddcb9e7f071e70a0b488dd3a7f39
Tatugh <tatu.ruohoaho@hotmail.fi>
2025-10-16 16:03:08 +0300

    Improved style of the ErrorPopup and refactored it so it can use plain string or object with multiple error messages Added cancel.svg icon

--- src/frontend/src/components/ErrorPopup/ErrorPopup.tsx
+++ src/frontend/src/components/ErrorPopup/ErrorPopup.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState, useRef } from 'react'
 import './ErrorPopup.scss'
+import CancelSVG from '../../assets/basicIcons/cancel.svg'
 import CloseSVG from '../../assets/basicIcons/close.svg'
 
 interface ErrorPopupProps {
@@ -7,6 +8,8 @@ interface ErrorPopupProps {
 }
 
 function ErrorPopup({message}: ErrorPopupProps) {
+   const [errors, setErrors] = useState<Object | string>("")
+   const [errorTitle, setErrorTitle] = useState<string>("")
    const [isShown, setIsShown] = useState<boolean>(false)
    const popUpDisplay = useRef<HTMLDivElement>(null)
 
@@ -16,18 +19,57 @@ function ErrorPopup({message}: ErrorPopupProps) {
 
    useEffect(() => {
       if (message && message?.length > 1){
+         parseErrorMessage(message)
          setIsShown(true)
-         console.log(message)
       }
    }, [message])
 
+   useEffect(() => {
+      if (errors){
+         console.log(errors)
+      }
+   }, [errors])
+
+   const parseErrorMessage = (msg: string | null) => {
+      if (typeof msg == "string"){
+         if (!msg.startsWith("{")){
+            setErrors(msg)
+            return
+         }
+         const tempMsg = JSON.parse(msg)
+         if ("title" in tempMsg){
+            setErrorTitle(tempMsg.title)
+         }else if ("message" in tempMsg){
+            setErrorTitle(tempMsg.message)
+         }
+         console.log(tempMsg.errors)
+         setErrors(tempMsg.errors)
+      }
+      // return msg?.split(" ").slice(1).join(" ")
+   }
 
    return (
       <>
          <div className={`error-popup ${isShown ? "show" : "hide"}`} ref={popUpDisplay}>
-            <img src={CloseSVG} />
-            <div id='error-msg'>{message}</div>
-            <div onClick={() => handleCloseClick()} className='close-btn'>x</div>
+            <div className='error-main'>
+
+               <div className='error-content-container'>
+                  <img src={CancelSVG} />
+                  <div className='error-content'>
+                     <div id='error-title'>Error{errorTitle ? ": " + errorTitle: ""}</div>
+                     {typeof errors == "object" ? Object.entries(errors).map(([key, error]) => (
+                        <div className='error-msg'>• {key}: {error}</div>
+                     )) : 
+                     <div className='error-msg'>{errors}</div>
+                     }
+                  </div>
+               </div>
+
+               <div onClick={() => handleCloseClick()} className='close-btn'>
+                  <img src={CloseSVG}/>
+               </div>
+
+            </div>
          </div>
       </>
    )
